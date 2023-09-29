0d3a4a06d9fbb24e558750041234772eec079e09
etiry <7997357+etiry@users.noreply.github.com>
2023-09-29 14:18:26 -0400

    Add react table library

--- client/src/components/Observations.jsx
+++ client/src/components/Observations.jsx
@@ -1,9 +1,24 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import CategoryDropdown from './CategoryDropdown';
-import { useGetCategoriesQuery } from '../slices/apiSlice';
+import {
+  Table,
+  Header,
+  HeaderRow,
+  Body,
+  Row,
+  HeaderCell,
+  Cell
+} from '@table-library/react-table-library/table';
+import { useTheme } from '@table-library/react-table-library/theme';
+import { getTheme } from '@table-library/react-table-library/baseline';
+import {
+  useGetCategoriesQuery,
+  useAssignCategoryMutation
+} from '../slices/apiSlice';
 
 const Observations = ({ observations, taxonomyId }) => {
+  const theme = useTheme(getTheme());
+  const data = { nodes: observations };
   const {
     data: categories,
     isLoading,
@@ -11,32 +26,61 @@ const Observations = ({ observations, taxonomyId }) => {
     isError,
     error
   } = useGetCategoriesQuery(taxonomyId);
+  const [assignCategory] = useAssignCategoryMutation();
+
+  const handleUpdate = async (observationId, event) => {
+    const queryParams = { observationId, categoryId: event.target.value };
+    try {
+      await assignCategory(queryParams);
+    } catch (error) {
+      console.log(`${error}`);
+    }
+  };
 
   if (isSuccess) {
     return (
       <Container>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHeading>Text</TableHeading>
-              <TableHeading>Category</TableHeading>
-              <TableHeading>Select Category</TableHeading>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {observations.map((ob) => (
-              <TableRow key={ob._id}>
-                <TableColumn>{ob.text}</TableColumn>
-                <TableColumn>{ob.category}</TableColumn>
-                <TableColumn>
-                  <CategoryDropdown
-                    categories={categories}
-                    observationId={ob._id}
-                  />
-                </TableColumn>
-              </TableRow>
-            ))}
-          </TableBody>
+        <Table data={data} theme={theme} layout={{ fixedHeader: true }}>
+          {(tableList) => (
+            <>
+              <Header>
+                <HeaderRow>
+                  <HeaderCell>Text</HeaderCell>
+                  <HeaderCell>Category</HeaderCell>
+                  <HeaderCell>Select a Category</HeaderCell>
+                </HeaderRow>
+              </Header>
+
+              <Body>
+                {tableList.map((item) => (
+                  <Row key={item._id} item={item}>
+                    <Cell>{item.text}</Cell>
+                    <Cell>{item.category}</Cell>
+                    <Cell>
+                      <select
+                        style={{
+                          width: '100%',
+                          border: 'none',
+                          fontSize: '1rem',
+                          padding: 0,
+                          margin: 0
+                        }}
+                        value={item.type}
+                        onChange={(event) => handleUpdate(item._id, event)}
+                      >
+                        <option value="">-- Select a category</option>
+                        {categories.map((category) => (
+                          <option key={category._id} value={category._id}>
+                            {category.name}
+                          </option>
+                        ))}
+                      </select>
+                    </Cell>
+                  </Row>
+                ))}
+              </Body>
+            </>
+          )}
         </Table>
       </Container>
     );
@@ -59,17 +103,6 @@ export default Observations;
 const Container = styled.div`
   max-height: 400px;
   overflow-y: scroll;
+  width: 100%;
   grid-row: 2 / end;
 `;
-
-const Table = styled.table``;
-
-const TableHeader = styled.thead``;
-
-const TableBody = styled.tbody``;
-
-const TableRow = styled.tr``;
-
-const TableHeading = styled.th``;
-
-const TableColumn = styled.td``;
