50641770e852ea72d47fa7d0dc77a3c6ec6a7878
etiry <7997357+etiry@users.noreply.github.com>
2023-10-06 10:21:11 -0400

    Update category assignment if already exists

--- controllers/observation.js
+++ controllers/observation.js
@@ -6,14 +6,22 @@ exports.assignCategory = async (req, res, next) => {
   const { observationId } = req.params;
 
   try {
-    await pool.query(
-      'INSERT INTO category_assignments (dataset_assignment_id, observation_id, category_id) VALUES ($1, $2, $3)',
-      [
-        parseInt(req.body.datasetAssignmentId),
-        parseInt(observationId),
-        parseInt(req.body.categoryId)
-      ]
+    const { rows: exists } = await pool.query(
+      'SELECT * FROM category_assignments WHERE dataset_assignment_id = $1 AND observation_id = $2',
+      [req.body.datasetAssignmentId, observationId]
     );
+
+    if (exists.length === 0) {
+      await pool.query(
+        'INSERT INTO category_assignments (dataset_assignment_id, observation_id, category_id) VALUES ($1, $2, $3)',
+        [req.body.datasetAssignmentId, observationId, req.body.categoryId]
+      );
+    } else {
+      await pool.query(
+        'UPDATE category_assignments SET category_id = $1 WHERE dataset_assignment_id = $2 AND observation_id = $3',
+        [req.body.categoryId, req.body.datasetAssignmentId, observationId]
+      );
+    }
 
     return res.status(200).end();
   } catch (error) {
