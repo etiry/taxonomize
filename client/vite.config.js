bda9f1575b73f15b08422186a9cd80c30df0380c
etiry <7997357+etiry@users.noreply.github.com>
2023-11-03 11:28:53 -0400

    Update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://taxonomize-2a1906a55331.herokuapp.com/',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
         ws: true
       },
       '/auth': {
-        target: 'http://localhost:5001',
+        target: 'https://taxonomize-2a1906a55331.herokuapp.com/',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/auth/, ''),
         secure: false,
