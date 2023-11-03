26ca311860c9f183c3f5503426ab28ea263cded6
etiry <7997357+etiry@users.noreply.github.com>
2023-11-03 10:33:29 -0400

    Commit prod env keys

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  PORT: process.env.PORT
+};
