5f8f931500b5391bd0202c43bc8532381811ce9b
etiry <7997357+etiry@users.noreply.github.com>
2023-11-03 10:36:31 -0400

    Use keys based on env

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,5 @@
 const jwt = require('jwt-simple');
-const keys = require('../config/dev');
+const keys = require('../config/keys');
 const { pool } = require('../dbHandler');
 const { setPassword } = require('../util/auth.js');
 
